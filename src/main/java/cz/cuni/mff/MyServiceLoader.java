1091959da6e055ce96f4b597f2755635962cd54e
Michal Kyjovsky <michalkyjovsky@hotmail.com>
2020-06-14 18:38:35 +0200

    Final

--- src/main/java/cz/cuni/mff/MyServiceLoader.java
+++ src/main/java/cz/cuni/mff/MyServiceLoader.java
@@ -1,53 +1,39 @@
 package cz.cuni.mff;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLClassLoader;
-import java.nio.file.Path;
 import java.util.*;
-import java.util.stream.Collectors;
+
 
 public class MyServiceLoader {
-
-    private static final String PROJECT_PATH = System.getProperty("user.dir") + "/" + "src/META-INF/services/";
+    private static final String META_INF = "META-INF/services/";
 
     public static <T> Iterable<T> load(Class<T> cl) {
-        try (BufferedReader br = new BufferedReader(new FileReader(PROJECT_PATH + cl.getName()))) {
-            List<T> clazzes = new ArrayList<>();
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                Enumeration<URL> urls = ClassLoader.getSystemResources("META-INF/services/" + line);
-                URLClassLoader urlClassLoader = URLClassLoader.newInstance(Collections.list(urls).toArray(new URL[0]));
-                Class<?> cls = Class.forName(line, true, urlClassLoader);
-                if (cl.isAssignableFrom(cls)) {
-                    clazzes.add(cl.cast(cls.getDeclaredConstructor().newInstance()));
+        List<T> plugins = new ArrayList<>();
+        try  {
+            ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+            Enumeration<URL> urls = classLoader.getResources(META_INF + cl.getCanonicalName());
+
+            while (urls.hasMoreElements()) {
+                URL service = urls.nextElement();
+
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(service.openStream()))) {
+                    String line;
+
+                    while ((line = br.readLine()) != null) {
+                        Class<?> cls = Class.forName(line);
+                        if (cl.isAssignableFrom(cls)) {
+                            plugins.add(cl.cast(cls.getDeclaredConstructor().newInstance()));
+                        }
+                    }
                 }
             }
-            return clazzes;
         } catch (IOException | ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
             e.printStackTrace();
         }
-        return null;
-    }
-
-    private static URLClassLoader getJarsFromClassPath() {
-        String[] classPathSplit = System.getProperty("java.class.path").split(";");
-        List<String> jars = Arrays
-                .stream(classPathSplit)
-                .filter(p -> p.endsWith(".jar"))
-                .collect(Collectors.toList());
-        List<URL> urls = new ArrayList<>();
-        for (String jar : jars) {
-            try {
-                urls.add(Path.of(jar).toUri().toURL());
-            } catch (MalformedURLException e) {
-                System.out.println("Pickle Tickle!");
-            }
-        }
-        return URLClassLoader.newInstance(urls.toArray(new URL [0]));
+        return plugins;
     }
 }
